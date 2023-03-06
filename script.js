c99738b6a1a655c941692cd217327882ac72d05b
OSTERITE <adrian@bandidos.org>
2023-03-06 13:43:51 +0100

    add_div method works

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+//Todo: File saver library
+
 class Content_card{
 
-    constructor(card_location){
-        this.card_location = card_location
+    constructor(card_location, card_id){
         //TODO: content of card can be stored in card?
+        this.card_location = card_location
+        this.location_element = document.getElementById(card_location)
+        
         this.card = document.createElement("div")
+        this.card.id = card_id
     }
 
     create_content_card(){
-        this.card_location.appendChild(card)
+        this.location_element.appendChild(this.card)
     }
 
-    add_image(location){}
+    add_image(target_location){}
 
-    add_div(location){}
+    add_div(target_location, div_id){
+        const div_location = document.getElementById(target_location)
+        console.log(div_location)
+        const div = document.createElement('div')
+        div.id = div_id
+        div_location.appendChild(div)
+    }
 
-    add_paragraph(location){}
+    add_paragraph(target_location){}
 
-    add_button(location, action){}
+    add_button(target_location, action){}
 
 }
 
@@ -32,10 +43,16 @@ function remove_button(element_id, content){
     target_element.removeChild(content)
 }
 
+window.onload = winInit;
+function winInit() {
+    const test_card = new Content_card("placeholder", "test_card_id")
+    test_card.create_content_card()
+    test_card.add_div("test_card_id", "test_div")
+}
 
-const skip_sang_knapp = document.createElement("button")
-const skip_sang_bilde = document.createElement("img");
-skip_sang_bilde.src = "./bilder/next_track_button_larger.png"
-skip_sang_bilde.id = ("skip_sang_bilde");
-skip_sang_knapp.removeChild(skip_sang_bilde); 
-skip_sang_knapp.addEventListener('click', () => {})
+// const skip_sang_knapp = document.createElement("button")
+// const skip_sang_bilde = document.createElement("img");
+// skip_sang_bilde.src = "./bilder/next_track_button_larger.png"
+// skip_sang_bilde.id = ("skip_sang_bilde");
+// skip_sang_knapp.removeChild(skip_sang_bilde); 
+// skip_sang_knapp.addEventListener('click', () => {})
